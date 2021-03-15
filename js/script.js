cef433c6b41f0643dc042ae6d9cf86f553494b80
Mark Lawrence Dilla <marcuslawrenzo@yahoo.com>
2021-03-15 23:19:47 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ $(document).on("click", ".click_me", function() {
     counter = counter + 1;
     if (counter == 3){
         alert('Told you I was useless!');
-        $('.click_me').popover('toggle');
+        $('.click_me').popover({
+            bs-content: "Told you I was useless!"
+        });
     }
     if (counter == 5){
         alert('Stop clicking. Nothing will happen!');
