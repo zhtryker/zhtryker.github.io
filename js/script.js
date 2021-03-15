80b43ad6a58f5e2d6bad42f5c5a2d09c5a1fd9d0
Mark Lawrence Dilla <marcuslawrenzo@yahoo.com>
2021-03-15 23:28:23 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,17 +3,25 @@ var counter = 0;
 $(document).on("click", ".click_me", function() {
     counter = counter + 1;
     if (counter == 3){
-        alert('Told you I was useless!');
+        //alert('Told you I was useless!');
         $('.click_me').popover({
             trigger: 'click',
             content: "Told you I was useless!"
         });
     }
     if (counter == 5){
-        alert('Stop clicking. Nothing will happen!');
+        //alert('Stop clicking. Nothing will happen!');
+        $('.click_me').popover({
+            trigger: 'click',
+            content: "Stop clicking. Nothing will happen!"
+        });
     }
     if (counter == 8){
-        alert('You are wasting your time bud!');
+        //alert('You are wasting your time bud!');
+        $('.click_me').popover({
+            trigger: 'click',
+            content: "You are wasting your time bud!"
+        });
         counter = 0;
     }
 });
