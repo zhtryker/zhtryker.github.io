c78ba1a3dca2e7407221c7b0d7a20244acd9c472
Mark Lawrence Dilla <marcuslawrenzo@yahoo.com>
2021-03-15 22:42:21 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+var counter = 0;
+
 $(document).on("click", ".click_me", function() {
-    alert('it works!');
+    counter = counter + 1;
+    if (counter == 3){
+        alert('it works!');
+        $('[data-toggle="popover"]').popover();
+    }
+    
 });
