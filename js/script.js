6b686bb5e9439efa97a7d651ebf2e5415d680139
Mark Lawrence Dilla <marcuslawrenzo@yahoo.com>
2021-03-16 19:58:37 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var counter = 0;
 
 $(document).on("click", ".click_me", function() {
     counter = counter + 1;
+    console.log(counter);
     if (counter == 3){
         //alert('Told you I was useless!');
         $('.click_me').attr('data-bs-content','Told you I was useless!');
@@ -15,7 +16,18 @@ $(document).on("click", ".click_me", function() {
     if (counter == 12){
         //alert('You are wasting your time bud!');
         $('.click_me').attr('data-bs-content','You are wasting your time bud!');
+        $('.click_me').popover('disable');
+    }
+    if (counter == 16){
         $('.click_me').popover('hide');
-        counter = 0;
+        $('body').attr('style', 'background-color: #fff !important');
+    }
+    if (counter == 17){
+        $('body').css('background-color', 'initial');
+        $('.click_me').attr('data-bs-content','... I guess I was wrong');
+        $('.click_me').popover('enable');
+    }
+    if (counter == 18){
+        $('.click_me').attr('data-bs-content','Yeah yeah, whatever.. Keep on clicking.');
     }
 });
