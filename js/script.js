14dba66ed9c598cb644f5d175badda284d8d837d
Mark Lawrence Dilla <marcuslawrenzo@yahoo.com>
2021-03-16 18:50:11 +0800

    add dynamic popover content

--- js/script.js
+++ js/script.js
@@ -2,26 +2,19 @@ var counter = 0;
 
 $(document).on("click", ".click_me", function() {
     counter = counter + 1;
+    console.log(counter);
     if (counter == 3){
         //alert('Told you I was useless!');
-        $('.click_me').popover({
-            trigger: 'click',
-            content: "Told you I was useless!"
-        });
+        $('.click_me').popover('show');
     }
+    
     if (counter == 5){
         //alert('Stop clicking. Nothing will happen!');
-        $('.click_me').popover({
-            trigger: 'click',
-            content: "Stop clicking. Nothing will happen!"
-        });
+        $('.click_me').attr('data-bs-content','Stop clicking. Nothing will happen!');
     }
     if (counter == 8){
         //alert('You are wasting your time bud!');
-        $('.click_me').popover({
-            trigger: 'click',
-            content: "You are wasting your time bud!"
-        });
+        $('.click_me').attr('data-bs-content','You are wasting your time bud!');
         counter = 0;
     }
 });
